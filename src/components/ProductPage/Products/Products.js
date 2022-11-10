9c372db42af56cd7e187b546752033b6f21cc2af
Ahmmed Rasel <rahmmed.info@gmail.com>
2022-11-10 14:25:56 +0600

    use reducer

--- src/components/ProductPage/Products/Products.js
+++ src/components/ProductPage/Products/Products.js
@@ -1,16 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import axios from 'axios';
+import React, { useEffect, useReducer } from 'react';
+import { Container, Row, Spinner } from 'react-bootstrap';
 import Product from '../Product/Product';
+import './products.css';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, products: action.payload };
+    case 'FETCH_FAILD':
+      return { ...state, loading: false, error: action.payload }
+    default:
+      return state;
+  }
+}
 const Products = () => {
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    loading: false,
+    error: '',
+    products: [],
+  })
 
-  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     const getProduct = async () => {
-      await fetch('http://localhost:8000/products')
-        .then(res => res.json())
-        .then(data => setProducts(data))
+      dispatch({ type: 'FETCH_REQUEST' })
+      try {
+        const products = await axios.get('http://localhost:8000/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: products.data })
+
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAILD', payload: error.message })
+      }
     }
     getProduct();
   }, [])
@@ -20,10 +43,14 @@ const Products = () => {
       <Container>
         <Row>
           {
-            products.map((item, index) => <Product
-              key={index}
-              product={item}
-            ></Product>)
+            loading ? <div className='loader'>
+              <Spinner animation="grow" />
+            </div>
+              :
+              products.map((item, index) => <Product
+                key={index}
+                product={item}
+              ></Product>)
           }
         </Row>
       </Container>
